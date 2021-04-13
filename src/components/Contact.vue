80703b5ac51d29a510859b169a9aa33e7afabc2b
Julie Gobin <julie.gobin1106@gmail.com>
2021-04-13 12:50:49 +0200

    formulaire contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,23 @@
     <div class="liste">
         <!-- <p>Téléphone : 06 07 82 42 12 <br/>
         <a href="mailto:julie.gobin1106@gmail.com">Email : julie.gobin1106@gmail.com</a><br/> -->
-        <a href="https://www.linkedin.com/in/julie-gobin-a6b1ab1a3/">LinkedIn</a>
+        <img src="../assets/linkedIn.png" alt="linkedIn" id="linkedIn"/><a href="https://www.linkedin.com/in/julie-gobin-a6b1ab1a3/">LinkedIn</a>
+    </div>
+    <div id="formulaire">
+      <form name="contact" method="POST" data-netlify="true">
+        <p class="flex-form">
+          <label>Votre nom: <input type="text" name="name" /></label>
+        </p>
+        <p class="flex-form">
+          <label>Votre Email: <input type="email" name="email" /></label>
+        </p>
+        <p class="flex-form">
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button id="envoyer" type="submit">Envoyer</button>
+        </p>
+      </form>
     </div>
   </section>
 </template>
@@ -26,9 +42,13 @@ export default {
 
 .contact {
     margin-top: 5%;
-    text-align: center;
     h3{
-    font-size: 1.5rem; 
+    font-size: 1.5rem;
+    text-align: center; 
+    }
+    #linkedIn{
+      width: 4%;
+      margin-right: 1%;
     }
   .liste{
     margin-top: 5%;
@@ -47,6 +67,38 @@ export default {
             text-decoration: underline;
             font-weight: 500;
         }
+    }
+  }
+  #formulaire{
+    margin-top: 10%;
+    text-align: center;
+    .flex-form{
+      display: flex;
+    }
+    input, textarea{
+      width: 100%;
+      height: 33px;
+      border: none;
+      background-color: $grey;
+      color: #000;
+    }
+    textarea{
+      height: 100px;
+    }
+    label{
+      color: $thirdColor;
+      font-weight: 700;
+      width: 100%;
+      text-align: left;
+    }
+    #envoyer{
+      width: 35%;
+      font-weight: 700;
+      background-color: $thirdColor;
+      color: $blanc;
+      border: none;
+      height: 40px;
+      margin-top: 3%;
     }
   }
 }
